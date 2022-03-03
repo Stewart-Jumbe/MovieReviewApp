ce7ee0a739337dca749bb4672196ba99aaa0d33c
Stewart <stewartjumbe@gmail.com>
2022-03-03 16:07:14 +0000

    updated pom

--- src/test/java/com/tsi/sjumbe/demoTest/SakilaMicroserviceApplicationTests.java
+++ src/test/java/com/tsi/sjumbe/demoTest/SakilaMicroserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.tsi.sjumbe.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SakilaMicroserviceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.tsi.sjumbe.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SakilaMicroserviceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
